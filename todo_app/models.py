0a469cecad89717f7dbd888bb9b60607cf17b2f7
Vin <maharjanebik061@gmail.com>
2022-10-04 12:22:43 +0545

    todo-app heading/description size increase

--- todo_app/models.py
+++ todo_app/models.py
@@ -5,8 +5,8 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Note(models.Model):
-    heading = models.CharField(max_length=30, help_text="Keep it brief.")
-    description = models.CharField(max_length=100, help_text="Full Description(OR short).")
+    heading = models.CharField(max_length=100, help_text="Keep it brief.")
+    description = models.CharField(max_length=500, help_text="Full Description(OR short).")
     date_added = models.DateField(auto_now_add=True)
     noteuser = models.ForeignKey(User,on_delete=models.CASCADE)
     
